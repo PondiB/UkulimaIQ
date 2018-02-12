dd26cea0bc794f6394049ff317ffadf4b16801dc
Pondi Brian <pondibrian@gmail.com>
2018-02-12 23:14:18 +0300

    Menu selection activity initiated

---
+++ app/src/main/java/com/example/pondibrian/ukulimaiq/MenuSelection.java
@@ -0,0 +1,13 @@
+package com.example.pondibrian.ukulimaiq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_selection);
+    }
+}
