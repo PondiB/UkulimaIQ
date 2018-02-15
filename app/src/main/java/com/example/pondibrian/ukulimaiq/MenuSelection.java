76049937289a20e07f95e4a7d95971bdaf7f20f4
Pondi Brian <pondibrian@gmail.com>
2018-02-15 12:20:01 +0300

    Menu switch case sorted

--- app/src/main/java/com/example/pondibrian/ukulimaiq/MenuSelection.java
+++ app/src/main/java/com/example/pondibrian/ukulimaiq/MenuSelection.java
@@ -2,12 +2,51 @@ package com.example.pondibrian.ukulimaiq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuSelection extends AppCompatActivity {
+public class MenuSelection extends AppCompatActivity  implements View.OnClickListener {
+    //Buttons Added
+    Button button2;
+    Button button3;
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_selection);
+
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(this);// calling onClick() method
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+
+            case R.id.button2:
+                // code for button when user clicks buttonOne.
+                break;
+
+            case R.id.button3:
+                // intent code
+                break;
+
+            case R.id.button4:
+                // intent code
+                break;
+
+            default:
+                break;
+
+        }
+
     }
 }
