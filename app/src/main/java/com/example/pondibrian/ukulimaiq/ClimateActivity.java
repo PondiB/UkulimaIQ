a1aea062fd2f0ac12e864cd14dd6b7c04a8fefb3
pondibrian <pondibrian@gmail.com>
2018-04-03 23:35:47 +0300

    Fetching data from openweather

--- app/src/main/java/com/example/pondibrian/ukulimaiq/ClimateActivity.java
+++ app/src/main/java/com/example/pondibrian/ukulimaiq/ClimateActivity.java
@@ -9,5 +9,9 @@ public class ClimateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climate);
+
+        DownloadTask task = new DownloadTask();
+
+        task.execute("http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=c908487c4f3bc9ee3802b490411e80ab");
     }
 }
