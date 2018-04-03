72e41eaf871fd9d0ecf8bdef7c4820d235a9feb8
pondibrian <pondibrian@gmail.com>
2018-04-03 22:35:01 +0300

    Download Task Java

---
+++ app/src/main/java/com/example/pondibrian/ukulimaiq/DownloadTask.java
@@ -0,0 +1,15 @@
+
+package com.example.pondibrian.ukulimaiq;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Pondi Brian on 4/3/2018.
+ */
+
+public class DownloadTask extends AsyncTask<String, Void, String>{
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
