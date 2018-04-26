d9e1afe959106c3946245b65fa32a9c03364c047
pondibrian <pondibrian@gmail.com>
2018-04-26 22:27:21 +0300

    weather review

--- app/src/main/java/com/example/pondibrian/ukulimaiq/ClimateActivity.java
+++ app/src/main/java/com/example/pondibrian/ukulimaiq/ClimateActivity.java
@@ -1,37 +1,17 @@
 package com.example.pondibrian.ukulimaiq;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.TextView;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
 
 public class ClimateActivity extends AppCompatActivity {
 
-    WebView ourBrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climate);
 
-        ourBrow = findViewById(R.id.webView);
-        ourBrow.setWebViewClient(new MyBrowser());
-        ourBrow.getSettings().setJavaScriptEnabled(true);
-        ourBrow.loadUrl("https://weather.com");
-    }
-    private class MyBrowser extends WebViewClient{
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+
     }
 }
