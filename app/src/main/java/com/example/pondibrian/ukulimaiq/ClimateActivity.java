9677dafe6b37d8fb680e7ec4b3f968cd1e126ec2
pondibrian <pondibrian@gmail.com>
2018-06-22 21:19:30 +0300

    Weather api

--- app/src/main/java/com/example/pondibrian/ukulimaiq/ClimateActivity.java
+++ app/src/main/java/com/example/pondibrian/ukulimaiq/ClimateActivity.java
@@ -3,8 +3,13 @@ package com.example.pondibrian.ukulimaiq;
 
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.webkit.WebResourceRequest;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
 
 public class ClimateActivity extends AppCompatActivity {
+
+    WebView ourBrowser;
 
 
     @Override
@@ -12,6 +17,19 @@ public class ClimateActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_climate);
 
+        ourBrowser = findViewById(R.id.webView);
+        ourBrowser.setWebViewClient(new MyBrowser());
+        ourBrowser.getSettings().setJavaScriptEnabled(true);
+        ourBrowser.loadUrl("http://www.weather.com");
 
+    }
+
+    private class MyBrowser extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            return super.shouldOverrideUrlLoading(view, request);
+
+        }
     }
 }
